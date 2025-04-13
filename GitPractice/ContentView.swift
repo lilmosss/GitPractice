36b7dab5bc99fc26376efa620fe215eceae72f58
Lily Mosisa <lilymosisa13@gmail.com>
2025-04-13 16:40:18 -0400

    Added another text view to show 2nd change

--- GitPractice/ContentView.swift
+++ GitPractice/ContentView.swift
@@ -14,6 +14,8 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Change 1!")
+            Text("Change 2.")
+            
         }
         .padding()
     }
