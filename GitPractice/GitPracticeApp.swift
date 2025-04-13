05c472b6b977fb43c640077da1d74284b0367239
Lily Mosisa <lilymosisa13@gmail.com>
2025-04-13 10:56:12 -0400

    Initial Commit

---
+++ GitPractice/GitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitPracticeApp.swift
+//  GitPractice
+//
+//  Created by Lily Mosisa on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct GitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitPracticeTests/GitPracticeTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitPracticeTests.swift
+//  GitPracticeTests
+//
+//  Created by Lily Mosisa on 4/13/25.
+//
+
+import Testing
+@testable import GitPractice
+
+struct GitPracticeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
